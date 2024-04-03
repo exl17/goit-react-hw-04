11e4eb22d6c4cc15e30c10d1d92c86995a4cd247
exl17 <143128264+exl17@users.noreply.github.com>
2024-04-04 00:51:51 +0300

    hw-4-fix-ImageCard

--- src/components/ImageCard/ImageCard.jsx
+++ src/components/ImageCard/ImageCard.jsx
@@ -3,9 +3,12 @@ import styles from './ImageCard.module.css';
 
 const ImageCard = ({ imageUrl, altText, onClick }) => {
   return (
-    <div className={styles.card} onClick={onClick}>
-      <img src={imageUrl} alt={altText} className={styles.image} />
-    </div>
+    <img
+      src={imageUrl}
+      alt={altText}
+      className={styles.image}
+      onClick={onClick}
+    />
   );
 };
 
